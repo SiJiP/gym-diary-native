5fe3f3c030defc419f9e8227a38504ce5680d945
Serhii Petruniv <spetru@softserveinc.com>
2021-06-22 10:44:03 +0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the Redux TypeScript template
+ * https://github.com/rahsheen/react-native-template-redux-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>My text work</Text>
+    </View>
+  );
+};
+
+export default App;
